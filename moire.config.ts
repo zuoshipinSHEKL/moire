8f6599cdf1cb34c183a0e6e6c196e5e6ca76b966
SHEKL <36996379+zuoshipinSHEKL@users.noreply.github.com>
2026-02-25 01:12:02 +0800

    update config

--- moire.config.ts
+++ moire.config.ts
@@ -1,10 +1,10 @@
 export const config = {
-  title: "Moire",
+  title: "",
   author: "Aeris",
   theme: "classic",
   pageSize: 20,
   order_by: "created",
   description: "Sync your thoughts from Apple Notes by Shortcuts.",
   keywords: "memo, thought, sync, apple notes, shortcuts, ios",
-  url: "https://moire.blog"
+  url: "https://hlae.cn/moire"
 };
